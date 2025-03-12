2eea37106f82291bde9567696ee060281b49f127
tapiaxavi <110013.clot@fje.edu>
2025-03-12 18:44:29 +0000

    Commit 3 FaseC

--- g15.sh
+++ g15.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-
-# Definir rutas
-WORKSPACE_DIR="/var/lib/jenkins/workspace/grupo15"
-DEST_DIR="/home/vagrant/app"
-
-# Asegurar que el directorio de destino existe
-mkdir -p "$DEST_DIR"
-
-# Copiar archivos .html y .php
-cp "$WORKSPACE_DIR"/*.html "$DEST_DIR"
-cp "$WORKSPACE_DIR"/*.php "$DEST_DIR"
-
-# Salida exitosa
+cp /var/lib/jenkins/workspace/grupo15/*.html /home/vagrant/app
+cp /var/lib/jenkins/workspace/grupo15/*.php /home/vagrant/app
 exit 0
-

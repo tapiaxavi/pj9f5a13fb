58468ed7a0b6df3e9c2a39be3dc716517315f352
tapiaxavi <110013.clot@fje.edu>
2025-03-17 16:46:09 +0000

    Commit final faseC

--- codi.php
+++ codi.php
@@ -9,43 +9,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $port_host = $_POST["port_host"];
     $port_vm = $_POST["port_vm"];
     $install_microk8s = isset($_POST["microk8s"]) && $_POST["microk8s"] === "yes";
+}
+
+// Script de aprovisionamiento base
+$provision_script = <<<EOT
+    sudo apt-get -y update
+    sudo apt-get -y install net-tools whois aptitude git zip unzip
+    sudo apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+    sudo apt-get update -y
+    sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose
+    sudo chown -R vagrant:vagrant /home/vagrant/pj9f4a86
+    sudo gpasswd -a vagrant docker
+EOT;
+
+// Agregar instalación de MicroK8s si se seleccionó
+if ($install_microk8s) {
+    $provision_script .= <<<EOT
 
 
-    // Script de aprovisionamiento base
-    $provision_script = <<<EOT
-        sudo apt-get -y update
-        sudo apt-get -y install net-tools whois aptitude git zip unzip
-        sudo apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-        curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-        sudo apt-get update -y
-        sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose
-        sudo chown -R vagrant:vagrant /home/vagrant/pj9f4a86
-        sudo gpasswd -a vagrant docker
+    # Instalación de MicroK8s
+    sudo snap install microk8s --classic
+    sudo usermod -a -G microk8s vagrant
+    sudo chown -R vagrant ~/.kube
     EOT;
+}
 
-
-    // Agregar instalación de MicroK8s si se seleccionó
-    if ($install_microk8s) {
-        $provision_script .= <<<EOT
-
-
-        # Instal·lació de MicroK8s
-        sudo snap install microk8s --classic
-        sudo usermod -a -G microk8s vagrant
-        sudo chown -R vagrant ~/.kube
-        EOT;
-    }
-
-
-    // Generar el Vagrantfile
-    $vagrantfile = <<<EOT
+// Generar el Vagrantfile
+$vagrantfile = <<<EOT
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
 
 # VARIABLES
-
 
 BOX_IMAGE = "debian/bookworm64"
 PROVIDER = "virtualbox"
@@ -60,8 +57,38 @@ PORT_FIS1 = $port_host
 PROT = "tcp"
 
 
-# CONFIGURACIÓ DE LA MÀQUINA
-
+# CONFIGURACIÓN DE LA MÁQUINA
 
 Vagrant.configure("2") do |config|
-    # BOX
+    config.vm.box = BOX_IMAGE
+    config.vm.provider PROVIDER do |vb|
+        vb.cpus = NUM_CPUS
+        vb.memory = MEMORIA_RAM
+        vb.name = NOM_MAQUINA
+    end
+
+    # Carpeta compartida
+    config.vm.synced_folder CARPETA_MAQ_FIS, CARPETA_MAQ_VIR
+
+    # Redireccionamiento de puertos
+    config.vm.network "forwarded_port", guest: PORT_VIR1, host: PORT_FIS1, protocol: PROT
+
+    # Configuración del hostname
+    config.vm.hostname = HOSTNAME
+
+    # Configuración del aprovisionamiento
+    config.vm.provision "shell", inline: <<-SHELL
+        $provision_script
+    SHELL
+end
+EOT;
+
+// Establecer los encabezados para forzar la descarga del archivo
+header('Content-Type: application/octet-stream');
+header('Content-Disposition: attachment; filename="Vagrantfile"');
+header('Content-Length: ' . strlen($vagrantfile));
+
+// Imprimir el contenido del Vagrantfile para que se descargue
+echo $vagrantfile;
+exit;
+?>

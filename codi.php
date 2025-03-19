53372b3209cb06fd3a62823e96e1b5581b6aab7c
tapiaxavi <110013.clot@fje.edu>
2025-03-19 18:55:58 +0100

    commit final gr15

--- codi.php
+++ codi.php
@@ -10,42 +10,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $port_vm = $_POST["port_vm"];
     $install_microk8s = isset($_POST["microk8s"]) && $_POST["microk8s"] === "yes";
 
-
     // Script de aprovisionamiento base
     $provision_script = <<<EOT
-        sudo apt-get -y update
-        sudo apt-get -y install net-tools whois aptitude git zip unzip
-        sudo apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-        curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-        sudo apt-get update -y
-        sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose
-        sudo chown -R vagrant:vagrant /home/vagrant/pj9f4a86
-        sudo gpasswd -a vagrant docker
-    EOT;
-
+sudo apt-get -y update
+sudo apt-get -y install net-tools whois aptitude git zip unzip
+sudo apt-get -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+sudo apt-get update -y
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose
+sudo chown -R vagrant:vagrant /home/vagrant/pj9f4a86
+sudo gpasswd -a vagrant docker
+EOT;
 
     // Agregar instalación de MicroK8s si se seleccionó
     if ($install_microk8s) {
         $provision_script .= <<<EOT
 
-
-        # Instal·lació de MicroK8s
-        sudo snap install microk8s --classic
-        sudo usermod -a -G microk8s vagrant
-        sudo chown -R vagrant ~/.kube
-        EOT;
+# Instal·lació de MicroK8s
+sudo snap install microk8s --classic
+sudo usermod -a -G microk8s vagrant
+sudo chown -R vagrant ~/.kube
+EOT;
     }
-
 
     // Generar el Vagrantfile
     $vagrantfile = <<<EOT
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-
 # VARIABLES
-
 
 BOX_IMAGE = "debian/bookworm64"
 PROVIDER = "virtualbox"
@@ -59,9 +53,29 @@ PORT_VIR1 = $port_vm
 PORT_FIS1 = $port_host
 PROT = "tcp"
 
-
 # CONFIGURACIÓ DE LA MÀQUINA
 
-
 Vagrant.configure("2") do |config|
-    # BOX
+    config.vm.box = BOX_IMAGE
+    config.vm.provider PROVIDER do |vb|
+        vb.memory = MEMORIA_RAM
+        vb.cpus = NUM_CPUS
+    end
+    config.vm.hostname = HOSTNAME
+    config.vm.network "private_network", type: "dhcp", auto_config: false, ip: "192.168.33.10"
+    config.vm.network "forwarded_port", guest: PORT_VIR1, host: PORT_FIS1, protocol: PROT
+    config.vm.synced_folder CARPETA_MAQ_FIS, CARPETA_MAQ_VIR
+    config.vm.provision "shell", inline: <<-SHELL
+        $provision_script
+    SHELL
+end
+EOT;
+
+    // Establecer cabeceras para forzar la descarga
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="Vagrantfile"');
+    echo $vagrantfile;
+    exit;
+}
+?>
+
